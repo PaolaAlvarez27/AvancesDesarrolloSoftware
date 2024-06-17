bd0fa1d322088a89f416be0a1de6e10b1ba91f54
Paola Alvarez <paalvarezb@correo.udistrital.edu.co>
2024-06-17 07:31:29 -0500

    Modificaciones controller, objectives; actualizaciones carpetas; import .sql

--- Actividad_1/objs/src/main/java/com/objectives/objs/model/pojo/Objectives.java
+++ Actividad_1/objs/src/main/java/com/objectives/objs/model/pojo/Objectives.java
@@ -27,7 +27,7 @@ import java.time.LocalDateTime;
 public class Objectives {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "Name", unique = true)
